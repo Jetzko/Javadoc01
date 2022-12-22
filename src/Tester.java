50c11f2655187c19894b03638c30968aec01b717
Jetzko <andreadefilippi68@gmail.com>
2022-12-22 15:21:02 +0100

    Javadoc1

---
+++ src/Tester.java
@@ -0,0 +1,13 @@
+/**
+ * This is our main class, by Andrea
+ */
+public class Tester {
+    public static void main(String[] args) {
+        /**
+         * A new object wordGames is created
+         */
+        WordGames wordGames = new WordGames();
+        wordGames.addHelloWord("Moto");
+        wordGames.getFullName("Mezio", "Fufezio");
+    }
+}
---
+++ src/WordGames.java
@@ -0,0 +1,23 @@
+/**
+ * This class, by Andrea, needs to enstablish methods for our objects.
+ */
+public class WordGames {
+    /**
+     * This method prints "Hello" + a word
+     * @param word
+     */
+    public void addHelloWord (String word){
+    System.out.println("Hello" + word);
+    }
+
+    /**
+     * This method prints a name and a surname
+     * @param name
+     * @param surname
+     */
+    public void getFullName (String name, String surname){
+        System.out.println(name + surname);
+    }
+
+}
+
